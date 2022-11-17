177c1f5bc8f37b9e02ffb374e5b5971ef21aaa94
sharhad <sharhad.bashar@uwaterloo.ca>
2022-11-17 02:23:51 -0500

    make training optional

--- python/main.py
+++ python/main.py
@@ -4,6 +4,7 @@ from train import Train
 from predict import Predict
 
 if __name__ == '__main__':
+	sys.argv[1] = sys.argv[1].lower() == 'true'
 	if (sys.argv[1]) == True:
 		print('Starting Training')
 		Train()
